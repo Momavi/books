6483edfc4dbf3af1fda84d1aa736f3fcb91f1e94
Momavi <68790545+Momavi@users.noreply.github.com>
2021-08-10 20:41:00 +0300

    Added API key

--- src/components/api/api.js
+++ src/components/api/api.js
@@ -6,9 +6,10 @@ const instance = axios.create({
 
 export const booksAPI = {
   getBooks(sortText = 'javascript', startIndex = 0, maxResults = 30) {
+    let keyAPI = 'AIzaSyCtNydhUF3PxH_n7KSa5WimBl_qLIwQyHg'
     let subject = sortText.categori === 'all' ?
       '' :
       `+subject:${sortText.categori}`
-    return instance.get(`volumes?q=${sortText.text}${subject}&orderBy=${sortText.sort}&startIndex=${startIndex}&maxResults=${maxResults}`)
+    return instance.get(`volumes?q=${sortText.text}${subject}&orderBy=${sortText.sort}&startIndex=${startIndex}&maxResults=${maxResults}&key=${keyAPI}`)
   }
 }
