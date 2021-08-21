99f7203fd30c065b2b4d8076d6f0659acd8b766e
Momavi <68790545+Momavi@users.noreply.github.com>
2021-08-21 19:51:07 +0300

    Updated App test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,16 @@
-import { render, screen } from '@testing-library/react';
 import App from './App';
+import { Provider } from 'react-redux';
+import store from './redux/store';
+import ReactDOM from 'react-dom';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+it('renders without crashing', () => {
+  const div = document.createElement('div');
+
+  ReactDOM.render(
+    <Provider store={store}>
+      <App />
+    </Provider>, div
+  );
+
+  ReactDOM.unmountComponentAtNode(div);
 });
